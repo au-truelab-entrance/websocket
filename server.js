4aa8525a25b1050852be5789ed44fa39f4c74d62
Jan Poonthong <janpoonthong628@gmail.com>
2023-12-27 20:52:46 +0700

    feat: console.log data emit from nextjs server

--- server.js
+++ server.js
@@ -16,8 +16,12 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
     console.log("a user connected");
+    
     socket.on("disconnect", () => {
         console.log("user disconnected");
+    });
+    socket.on('message', (data) => {
+        console.log('Received message:', data);
     });
 });
 
